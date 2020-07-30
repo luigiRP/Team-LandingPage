19e05f5af81866f335fdf07a56d07fb3b9a48cd8
luigiRP <luigirosales84@gmail.com>
2020-07-30 09:32:36 +0000

    segundo

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Jumbotron() {
 	return (
-		<div class="jumbotron container my-5">
-			<h1 class="display-4">A Warm Welcome!</h1>
+		<div className="jumbotron container my-5">
+			<h1 className="display-4">A Warm Welcome!</h1>
 			<p>
 				It uses utility classes for typography and spacing to space
 				content out within the larger container.
 			</p>
-			<p class="lead">
-				<a class="btn btn-primary btn-lg" href="#" role="button">
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
 					Learn more
 				</a>
 			</p>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,30 +2,30 @@ import React from "react";
 
 export default function Navbar() {
 	return (
-		<div class="container-fluid bg-dark">
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark container">
-				<a href="#" class="navbar-brand">
+		<div className="container-fluid bg-dark">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark container">
+				<a href="#" className="navbar-brand">
 					Start Bootstrap
 				</a>
-				<div class="collapse navbar-collapse">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a href="#" class="nav-link text-white">
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<a href="#" className="nav-link text-white">
 								Home
 							</a>
 						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">
+						<li className="nav-item">
+							<a href="#" className="nav-link">
 								About
 							</a>
 						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">
+						<li className="nav-item">
+							<a href="#" className="nav-link">
 								Services
 							</a>
 						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">
+						<li className="nav-item">
+							<a href="#" className="nav-link">
 								Contact
 							</a>
 						</li>
